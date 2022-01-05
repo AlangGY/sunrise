e985062e759f8f0dd4a5a18b5f4a13aae949ed08
Alang Kim <rlaangh77@naver.com>
2022-01-05 17:02:50 +0900

    feat: 기본 동작 로직

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { sunRiseAPI } from './lib/api';
 
 function App() {
-  return <div className='App'>Hello React!</div>;
+  const [sunriseAPIState, setSunriseAPIState] = useState({
+    value: null,
+    isLoading: false,
+    error: null
+  });
+  useEffect(() => {
+    setSunriseAPIState(state => ({ ...state, isLoading: true }));
+    sunRiseAPI
+      .getSunriseDate()
+      .then(data =>
+        setSunriseAPIState({
+          value: data.results.sunrise,
+          isLoading: false,
+          error: null
+        })
+      )
+      .catch(e =>
+        setSunriseAPIState({ value: null, isLoading: false, error: e })
+      );
+  }, []);
+
+  const date = useMemo(
+    () => new Date(sunriseAPIState.value),
+    [sunriseAPIState]
+  );
+
+  return (
+    <div className='App'>
+      {sunriseAPIState.isLoading
+        ? '시간 정보를 불러오는 중...'
+        : date.toLocaleTimeString()}
+    </div>
+  );
 }
 
 export default App;
