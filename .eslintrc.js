61abf4443236e7ada94846a88bd30f450f3ab07d
Alang Kim <rlaangh77@naver.com>
2022-01-05 17:02:26 +0900

    fix: lint 룰 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,36 +16,18 @@ module.exports = {
     'no-var': 'error',
     semi: 'error',
     'prefer-const': 'error',
-    'prefer-destructuring': [
-      'error',
-      {
-        array: false,
-        object: true
-      },
-      { enforceForRenamedProperties: true }
-    ],
     'no-self-assign': ['error', { props: true }],
-    'object-property-newline': [
-      'error',
-      { allowAllPropertiesOnSameLine: false }
-    ],
     'object-shorthand': ['error', 'always'],
     eqeqeq: ['error', 'always'],
     'lines-between-class-members': ['error', 'always'],
-    'array-bracket-spacing': ['error', 'never'],
-    'space-in-parens': ['error', 'never'],
     'no-new-func': 'error',
     'prefer-arrow-callback': 'error',
     'arrow-parens': ['error', 'as-needed'],
-    'implicit-arrow-linebreak': ['error', 'beside'],
     'newline-before-return': 'error',
-    'template-curly-spacing': ['error', 'never'],
     'prefer-template': 'error',
     'space-infix-ops': 'error',
-    'default-case': ['error', { commentPattern: '^no\\sdefault' }],
     'no-fallthrough': ['error'],
     'no-multiple-empty-lines': 'error',
-    'eol-last': 'error',
-    'no-multi-spaces': 'error'
+    'eol-last': 'error'
   }
 };
